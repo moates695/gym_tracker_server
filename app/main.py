e81ab2a015c19e024d72ffbd73abcaf7676ca7b4
Marcus Oates <marcus.oates@downergroup.com>
2025-05-10 15:30:30 +1000

    get exercises

--- app/main.py
+++ app/main.py
@@ -8,11 +8,13 @@ from fastapi import FastAPI
 
 from api.routes import register
 from api.routes import auth
+from api.routes import exercises
 
 app = FastAPI(title="Gym Tracker API")
 
 app.include_router(register.router)
 app.include_router(auth.router)
+app.include_router(exercises.router)
 
 if __name__ == "__main__":
     import uvicorn
