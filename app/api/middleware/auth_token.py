8a9551ae037b0856767f471a4f5ce09439f03cd2
Marcus Oates <marcus.oates@downergroup.com>
2025-09-13 09:14:54 +1000

    start work

--- app/api/middleware/auth_token.py
+++ app/api/middleware/auth_token.py
@@ -2,6 +2,10 @@ from datetime import datetime, timedelta, timezone
 import jwt
 import os
 
+from .misc import load_env_vars
+
+load_env_vars()
+
 def generate_token(email, user_id, days=0, minutes=0, is_temp=False):
     utc_now = datetime.now(timezone.utc)
     payload = {
@@ -19,4 +23,7 @@ def is_token_expired(token):
     return datetime.now(timezone.utc) > datetime.fromtimestamp(token["exp"], timezone.utc)
 
 def get_env_value(is_temp):
-    return os.getenv("SECRET_KEY" if not is_temp else "TEMP_SECRET_KEY")
+    return os.getenv("SECRET_KEY" if not is_temp else "TEMP_SECRET_KEY")  
+
+if __name__ == "__main__":
+   print(generate_token('moates695@gmail.com', 'a8bf1a23-33f0-4b52-9d9b-7bfde7eea36a', days=1))
