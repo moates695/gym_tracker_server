d1964da75682fe7ab28a3dbd4793e51af8f4dd4d
Marcus Oates <marcus.oates@downergroup.com>
2025-10-02 16:43:17 +1000

    add favourite stats

--- app/api/routes/stats.py
+++ app/api/routes/stats.py
@@ -251,7 +251,6 @@ async def stats_history(credentials: dict = Depends(verify_token)):
     finally:
         if conn: await conn.close()
 
-# todo order return group/target names
 @router.get("/stats/distributions")
 async def stats_history(credentials: dict = Depends(verify_token)):
     try:
@@ -301,6 +300,65 @@ async def stats_history(credentials: dict = Depends(verify_token)):
 
         return {
             "distributions": dict(sorted(distributions.items()))
+        }
+
+    except HTTPException as e:
+        return JSONResponse(status_code=e.status_code, content={"detail": e.detail})
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Uncaught exception")
+    finally:
+        if conn: await conn.close()
+
+# todo favourite exercises per muscle group (choose with activation level over 7)
+@router.get("/stats/favourites")
+async def stats_history(credentials: dict = Depends(verify_token)):
+    try:
+        conn = await setup_connection()
+
+        total_rows = await conn.fetch(
+            """
+            select *
+            from exercise_totals
+            where user_id = $1
+            """, credentials["user_id"]
+        )
+
+        exercise_name_rows = await conn.fetch(
+            """
+            select *
+            from exercise_base_variants
+            """
+        )
+        name_map = {}
+        for row in exercise_name_rows:
+            name_map[str(row["base_id"])] = {
+                "exercise_name": row["base_name"],
+                "variation_name": None
+            }
+            if row["variant_id"] is None: continue
+            name_map[str(row["variant_id"])] = {
+                "exercise_name": row["base_name"],
+                "variation_name": row["variant_name"]
+            }
+
+        data = []
+        for total_row in total_rows:
+            exercise_id = str(total_row["exercise_id"])
+            data.append({
+                "exercise_id": exercise_id,
+                "exercise_name": name_map[exercise_id]["exercise_name"],
+                "volume": total_row["volume"],
+                "num_sets": total_row["num_sets"],
+                "reps": total_row["reps"],
+                "counter": total_row["counter"],
+            })
+            variation_name = name_map[exercise_id]["variation_name"]
+            if variation_name is None: continue
+            data[-1]["variation_name"] = variation_name
+
+        return {
+            "favourites": data
         }
 
     except HTTPException as e:
