59d9bac35e998d697e13c237ad25f488c9b36602
Marcus Oates <marcus.oates@downergroup.com>
2025-12-11 20:48:13 +1100

    save progress

--- app/api/routes/users/friends.py
+++ app/api/routes/users/friends.py
@@ -21,6 +21,46 @@ router = APIRouter()
 #   remove friends
 #   look at friends stats?
 
-@router.get("/users/search")
-async def users_search(credentials: dict = Depends(verify_token)):
-    return {}
+@router.get("/search")
+async def users_search(username: str, credentials: dict = Depends(verify_token)):
+    try:
+        conn = await setup_connection()
+
+        rows = await conn.fetch(
+            """
+            select
+                u.*,
+                (f.user1_id is not null or f.user2_id is not null) as is_friend
+            from users u
+            left join friends f
+                on (f.user1_id = u.id and f.user2_id = $1)
+                or (f.user2_id = u.id and f.user1_id = $1)
+            where id != $1
+            and username ilike $2 || '%'
+            and id not in (
+                select blocked_id
+                from blocked_users
+                where victim_id = $1
+            )
+            order by username
+            limit 50
+            """,
+            credentials["user_id"],
+            username.strip()
+        )
+
+        return {
+            "matches": [{
+                "id": row["id"],
+                "username": row["username"],
+                "is_friend": row["is_friend"]
+            } for row in rows]
+        }
+
+    except SafeError as e:
+        raise e
+    except Exception as e:
+        print(str(e))
+        raise Exception('uncaught error')
+    finally:
+        if conn: await conn.close()
--- app/api/routes/users/router.py
+++ app/api/routes/users/router.py
@@ -3,10 +3,13 @@ from fastapi import APIRouter
 from app.api.routes.users import data_history
 from app.api.routes.users import get_data
 from app.api.routes.users import update_data
-
+from app.api.routes.users import friends
+from app.api.routes.users import permissions
 
 router = APIRouter(prefix="/users")
 
 router.include_router(data_history.router)
 router.include_router(get_data.router)
 router.include_router(update_data.router)
+router.include_router(friends.router)
+router.include_router(permissions.router)
