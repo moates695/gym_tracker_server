8fcd9bc203362c1ff22d87ac91a0332d24cc81a0
Marcus Oates <marcus.oates@downergroup.com>
2025-12-27 20:48:45 +1100

    working through it

--- app/api/routes/home/home.py
+++ app/api/routes/home/home.py
@@ -32,6 +32,7 @@ async def exercise_history(credentials: dict = Depends(verify_token)):
             left join muscle_groups_targets mgt
             on mgt.target_id = emt.muscle_target_id
             where w.user_id = $1
+            order by mgt.group_name, mgt.target_name
             """,
             credentials["user_id"],
         )
@@ -94,18 +95,18 @@ async def exercise_history(credentials: dict = Depends(verify_token)):
                 data[span][group]["targets"][target]["sets"] += row["num_sets"]
                 data[span][group]["targets"][target]["reps"] += row["reps"]
     
-        muscle_maps = await get_muscle_maps()
-        group_to_targets = muscle_maps["group_to_targets"]
+        # muscle_maps = await get_muscle_maps()
+        # group_to_targets = muscle_maps["group_to_targets"]
 
-        for span, group_data in data.items():
-            for group, targets in group_to_targets.items():
-                if group not in group_data:
-                    group_data[group] = deepcopy(empty) | {
-                        "targets": {}
-                    }
-                for target in targets:
-                    if target in group_data[group]["targets"]: continue
-                    group_data[group]["targets"][target] = deepcopy(empty)
+        # for span, group_data in data.items():
+        #     for group, targets in group_to_targets.items():
+        #         if group not in group_data:
+        #             group_data[group] = deepcopy(empty) | {
+        #                 "targets": {}
+        #             }
+        #         for target in targets:
+        #             if target in group_data[group]["targets"]: continue
+        #             group_data[group]["targets"][target] = deepcopy(empty)
 
 
         return {
