2c9ce7ba6b9348c1cf79352d257c6441de0b8dd6
Marcus Oates <marcus.oates@downergroup.com>
2025-12-31 16:56:19 +1100

    fix online friends sql

--- sql/online_users.sql
+++ sql/online_users.sql
@@ -1,22 +1,26 @@
 
-CREATE TABLE public.overall_leaderboard (
+CREATE TABLE public.online_users (
     user_id uuid NOT NULL,
-    volume real NOT NULL,
-    num_sets integer NOT NULL,
-    reps integer NOT NULL,
-    num_exercises integer NOT NULL,
-    num_workouts integer NOT NULL,
-    duration_mins integer NOT NULL,
-    last_updated timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc'::text) NOT NULL
+    is_online boolean NOT NULL
 );
 
 
-ALTER TABLE public.overall_leaderboard OWNER TO postgres;
+ALTER TABLE public.online_users OWNER TO postgres;
 
 --
--- TOC entry 4966 (class 2606 OID 241772)
--- Name: overall_leaderboard overall_leaderboard_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 4966 (class 2606 OID 315579)
+-- Name: online_users online_users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.overall_leaderboard
-    ADD CONSTRAINT overall_leaderboard_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+ALTER TABLE ONLY public.online_users
+    ADD CONSTRAINT online_users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- TOC entry 4967 (class 2606 OID 315580)
+-- Name: online_users online_users_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.online_users
+    ADD CONSTRAINT online_users_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+
